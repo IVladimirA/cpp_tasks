d52e9c1a6a7694f86178003ea0fc4ac1ae6fe1cd
Vladimir Ivanov <ivanov.vladimir102002@gmail.com>
2022-09-17 11:50:09 +0300

    fix digit value calculation

--- 2/insurance/main.cpp
+++ 2/insurance/main.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 
-uint64_t digitSum(uint64_t number)
+uint64_t digitvalue(std::string number)
 {
     uint64_t result = 0;
-    while (number > 0) {
-        result += number % 10;
-        number /= 10;
+    for (size_t charIdx = 0; charIdx < number.size(); ++charIdx) {
+        result += (number.size() - charIdx) * (uint64_t)(number[charIdx] - '0');
     }
     return result;
 }
 
-bool checkInsurance(uint64_t mainNumber, uint64_t controlNumber)
+bool checkInsurance(std::string mainNumber, uint64_t controlNumber)
 {
-    uint64_t sum = digitSum(mainNumber);
-    sum %= 101;
-    return sum == controlNumber || (sum == 100 && controlNumber == 0);
+    uint64_t digits = digitvalue(mainNumber);
+    digits %= 101;
+    return digits == controlNumber || (digits == 100 && controlNumber == 0);
 }
 
 int main() {
     using namespace std;
-    uint64_t mainNum, controlNum;
+    string mainNum;
+    uint64_t controlNum;
     cin >> mainNum >> controlNum;
     cout << checkInsurance(mainNum, controlNum) << '\n';
     return 0;
